c65f36c4d0b7a6f3798fd5be6da275e4dcdcfbbc
SabbirRahman71 <mdsabbirrahmanbd71@gmail.com>
2024-09-25 01:01:23 +0600

    footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
+//import App from "./App.jsx";
 import "./index.css";
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
